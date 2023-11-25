55d8244006b0d0541ef345e0604e49a883dfa9c0
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2023-11-24 20:14:23 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const scuberDrivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = () => {
+        return scuberDrivers.slice(0, 2)
+    }
+
+const returnLastTwoDrivers = function() {
+    return scuberDrivers.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    return function(y) {
+        return x * y; 
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+fareDoubler(2);
+
+/*const fareDoubler = function(a) {
+    return createFareMultiplier(2)(1) * a;
+};*/
+
+const fareTripler = createFareMultiplier(3);
+fareTripler(3);
+
+function selectDifferentDrivers() {
+    const randomIndex = Math.random();
+
+    if (randomIndex > .5) {
+        return returnFirstTwoDrivers()
+    } else {
+        return returnLastTwoDrivers()
+    }    
+};
